2aa88b6e5d8ff11e0dd75313cefbc81091f1a327
ttakino <taka3.3chan88@gmail.com>
2024-06-30 17:44:42 +0900

    挿入ソートができた。ノルマクリアだどん！

--- insertion_sort.c
+++ insertion_sort.c
@@ -6,7 +6,7 @@
 /*   By: ttakino <ttakino@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 17:54:42 by ttakino           #+#    #+#             */
-/*   Updated: 2024/06/27 18:49:05 by ttakino          ###   ########.fr       */
+/*   Updated: 2024/06/30 17:26:03 by ttakino          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,43 +19,57 @@ long	get_max(t_stack *light, int direction)
 	int		group;
 
 	max = LONG_MIN;
-	target = set_next_node(light, direction);
-	group = target->group;
+	target = light->next;
+	group = set_next_node(light, direction)->group;
 	while (target->group == group)
 	{
 		if (max < target->data)
 			max = target->data;
-		target = set_next_node(target, direction);
-	}
-	// printf("max = %ld\n", max);
-	return (max);
-}
-
-int	calculate_push_cost(t_stack *light, long max, int direction)
-{
-	int		group;
-	int		cnt;
-	t_stack	*target;
-
-	group = set_next_node(light, direction);
-	cnt = 0;
-	target = light->next;
-	while (target->group == group)
-	{
-		if (target->data == max)
-			break;
-		cnt++;
 		target = target->next;
 	}
 	target = light->prev;
 	while (target->group == group)
 	{
-		if (target->data == max)
-			break;
-		cnt--;
+		if (max < target->data)
+			max = target->data;
 		target = target->prev;
 	}
-	if (cnt <= 0)
+	// printf("max = %ld\n", max);
+	return (max);
+}
+
+int	set_next_direction(t_stack *light, int direction)
+{
+	if (light->next->group > light->prev->group)
+		return (CW);
+	else if (light->next->group == light->prev->group)
+		return (direction);
+	else
+		return (CCW);
+}
+
+int	calculate_push_cost(t_stack *light, long max, int direction)
+{
+	t_stack	*target;
+	int		cnt;
+	int		size;
+
+
+	max = get_max(light, direction);
+	// target = set_next_node(light, direction);
+	size = count_stack_size(light, STACK, direction) / 2 + 1;
+	// printf("sise = %d\n", size);	
+	target = light->next;
+	cnt = 0;
+	while (target->group != light->group)
+	{
+		cnt++;
+		if (target->data == max)
+			break;
+		target = target->next;
+	}
+	// printf("cnt = %d\n", cnt);
+	if (cnt < size)
 		return (CW);
 	else
 		return (CCW);
@@ -65,7 +79,7 @@ void	insertion_sort(t_stack *light, t_stack *dark, int direction)
 {
 	int		size;
 	long	max;
-	// t_stack	*target;
+	int		cost;
 
 	if (light->group != B)
 	{
@@ -73,22 +87,31 @@ void	insertion_sort(t_stack *light, t_stack *dark, int direction)
 		return ;
 	}
 
+	// printf("direction = %d\n", direction);
 	size = count_stack_size(light, GROUP, direction);
-	// max = set_next_node(light, direction)->data;
 	max = get_max(light, direction);
 	// printf("max = %ld\n", max);
-	// target = set_next_node(light, direction);
+	cost = calculate_push_cost(light, max, direction);
+	// __print_stack(light);
+	// printf("cost = %d\n", cost);
 	while (0 < size--)
 	{
-		if (direction == CCW)
+		if (cost == CCW)
 			r_rotate(light, TRUE);
 		if (light->next->data == max)
+		{
 			push(light, dark);
-		else if (direction == CW)
+			break ;
+		}
+		else if (cost == CW)
 			rotate(light, TRUE);
 	}
-	if (dark->next->group != set_next_node(light, direction)->group)
+	if (dark->next->group != light->next->group
+		&& dark->next->group != light->prev->group)
+	{
+		// printf("\x1b[34mreturn\n\x1b[0m");
 		return ;
+	}
 	else
-		insertion_sort(light, dark, direction * -1);
+		insertion_sort(light, dark, set_next_direction(light, direction));
 }
